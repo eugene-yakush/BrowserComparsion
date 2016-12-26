0a3c6ec6e4fbd1280bba5469b00fa85979ada8ae
Eugene Yakush <eugene.yakush@teamdev.com>
2016-12-26 17:56:43 +0200

    Table project added to solution

---
+++ Comparsion/Scripts/samples.js
@@ -0,0 +1,18 @@
+﻿function LoadComparsion(caption, path)
+{
+    var fullpath = 'Content/Samples/' + path;
+    //alert('' + caption + ':' + fullpath);
+
+    document.getElementById('sample_title').innerText = caption;
+    document.getElementById('sample_body').innerHTML = '<img src="Content/Images/ajax-loader.gif">';
+
+    $.ajax({
+        url: fullpath,
+        success: function(data)
+        {
+            var hl = Prism.highlight(data, Prism.languages.csharp);
+            document.getElementById('sample_body').innerHTML = hl;
+        },
+        error: function(a,b,c) { document.getElementById('sample_body').innerHTML = "Unable to find source"; }
+    });
+}
